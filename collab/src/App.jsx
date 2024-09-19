b532dda3a2c51e279b9d51459f885cff80bef687
AdieDev0 <devutella@gmail.com>
2024-09-19 23:41:44 +0800

    test123

---
+++ collab/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hello from './Components/Hello'
+import Hi from './Components/Hi'
+
+const App = () => {
+  return (
+    <div>
+      <Hello/>
+      <Hi/>
+    </div>
+  )
+}
+
+export default App
